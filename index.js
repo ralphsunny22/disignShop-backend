840c10aee4ff66676d156eb32510ebd243b02e35
ralphsunny22 <ralphsunny114@gmail.com>
2022-04-30 18:47:10 +0100

    install & integrated mongoose-db & dotenv

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const mongoose = require("mongoose")
+
 
 //making our appln an object
 const app = express()
+
+require("dotenv").config()
 
 //express.json() is nw a middleware fxn, which helps to expand our application
 app.use(express.json())
@@ -13,5 +17,15 @@ app.get("/", (req, res) => {
 });
 
 const port = process.env.PORT || 5000
+const db_uri = process.env.DB_URI
 
 app.listen(port, console.log(`Server running on port ${port}`))
+
+//connecting to mongodb
+mongoose
+  .connect(db_uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log("MongoDB connection established..."))
+  .catch((error) => console.error("MongoDB connection failed:", error.message));
