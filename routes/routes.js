e5cad9d938d550c47d32c7fd69ae1b54ddb4a387
ralphsunny22 <ralphsunny114@gmail.com>
2022-05-04 11:11:09 +0100

    edit product

--- routes/routes.js
+++ routes/routes.js
@@ -4,13 +4,15 @@ const Product = require("../models/product");
 const multer = require("multer");
 const path = require("path");
 const Swal = require('sweetalert2');
+const { User } = require("../models/user");
+const fs = require('fs')
 
 //create router
 let router = express.Router();
 //store product img
 const Storage = multer.diskStorage({
     destination: (req, file, cb) => {
-        cb(null, './uploads');
+        cb(null, './public/uploads');
     },
     filename: (req, file, cb) => {
         //cb(null, Date.now()+path.extname(file.originalname)); //123456.png
@@ -40,7 +42,7 @@ router.post("/add-product", upload, (req, res) => {
             res.json({message: err.message, type:'danger'})
         } else {
             req.flash('add_post_flash', 'Post added successfully')
-            res.redirect("/add-product");
+            res.redirect("/all-product");
         }
     })
 })
@@ -49,8 +51,75 @@ router.get("/", (req, res) => {
     res.render('landing', { title: 'Admin Dashboard' })
 })
 router.get("/add-product", (req, res) => {
-    res.render('addProduct', { title: 'Add Product', addPostFlash: req.flash('add_post_flash') })
+    res.render('addProduct', { title: 'Add Product' })
 })
+router.get("/all-product", (req, res) => {
+    Product.find().exec((err, products) => {
+        if(err){
+            res.json({ message: err.message })
+        } else {
+            res.render('allProduct', { 
+                title: 'All Product',
+                products: products,
+                addPostFlash: req.flash('add_post_flash'), 
+            })
+        }
+    })
+})
+
+// Edit single product
+router.get('/edit/:id', (req, res) => {
+    let id = req.params.id;
+    Product.findById(id, (err, product) => {
+        if(err){
+            res.redirect('/');
+        } else {
+            if(product == null){
+                res.redirect('/');
+            } else {
+                res.render('editProduct', {
+                    title: "Edit Product",
+                    product: product,
+                })
+            }
+        }
+    })
+})
+
+// update product route
+router.post('/update-product/:id', upload, (req, res) => {
+    let id = req.params.id;
+    let new_image = '';
+
+    if(req.file){
+        new_image = req.file.filename
+        try{
+            fs.unlinkSync('./public/uploads/'+req.body.old_image);
+        } catch (err) {
+            console.log(err);
+        }
+    } else {
+        new_image = req.body.old_image;
+    }
+
+    Product.findByIdAndUpdate(id, {
+        title: req.body.title,
+        price: req.body.price,
+        cancelled_price: req.body.cancelled_price,
+        image: new_image,
+        description: req.body.description,
+        category: req.body.category,
+        tags: req.body.tags,
+    }, (err, result) => {
+        if(err){
+            res.json({ message: err.message, type: 'danger' })
+        } else {
+            req.flash('add_post_flash', 'Post Updated Successfully!')
+            res.redirect("/all-product");
+        }
+    })
+
+});
 
 module.exports = router;
 // export default router;
